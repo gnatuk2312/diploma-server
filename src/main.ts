89b0cf1c80d878392717f51a7412f428170daff7
Hnat Liashenko <hnat.liashenko@bluepes.com>
2025-03-25 12:40:04 +0200

    fix: enable cors policy

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
